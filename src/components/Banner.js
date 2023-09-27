f0a180eecbbeefdc2a78e5ad1b44bb1dd7292c0d
lalith kishore <lalitkishorep.20cse@kongu.edu>
2023-09-27 17:45:01 +0530

    changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,10 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm Yazeer`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>"Unlocking the Digital World's Beauty: Meet the UI/UX Designer! 🎨✨
+Crafting seamless digital experiences, they blend artistry with functionality. With every pixel, they paint a world where user journeys are intuitive, and interfaces are as captivating as they are efficient. Merging creativity and user psychology, they sculpt designs that leave lasting impressions. Elevate your digital presence with a UI/UX Designer and watch your vision come to life!"
+</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
